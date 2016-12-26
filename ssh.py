93a8f58bee1da68b79a0adfcc1f1347e1ff1f8a5
Yaroslav <yarmasich@gmail.com>
2016-12-26 22:06:33 +0200

    fix bag

--- ssh.py
+++ ssh.py
@@ -4,12 +4,12 @@ print("""Select server:
 1: Plisko
 2: Nodejs""")
 
-user = "plisko"
-passwd = "159753cfthn"
+user = None
+passwd = None
 domain = None
 adduser = None
 userpass = None
-plisko = "10.10.10.27"
+plisko = "95.46.44.18"
 nodejs = "10.10.10.32"
 port = 22
 client = paramiko.SSHClient()
@@ -17,31 +17,31 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 choice = input("Server:")
 if choice == "1":
-    #user = input("Enter username: ")
-    #passwd = input("Enter password: ")
+    user = input("Enter username: ")
+    passwd = input("Enter password: ")
     adduser = input("Enter neme new user: ")
     domain = input("Enter domain name: ")
     # userpass = input("Enter username password: ")
     client.connect(hostname=plisko, username=user, password=passwd, port=port)
-    stdin, stdout, stderr = client.exec_command('sudo -S useradd -m -d /home/' + adduser + ' ' + '-s /bin/bash -G sudo' + ' ' + adduser)
-    stdin, stdout, stderr = client.exec_command('sudo -S mkdir /home/' + adduser)
+    client.exec_command('sudo -S useradd -m -d /home/' + adduser + ' ' + '-s /bin/bash -G sudo' + ' ' + adduser)
+    client.exec_command('sudo -S mkdir /home/' + adduser)
     #stdin, stdout, stderr = client.exec_command('passwd' + ' ' + adduser)
-    stdin, stdout, stderr = client.exec_command('sudo -S mkdir /home/' + adduser + '/www')
-    stdin, stdout, stderr = client.exec_command('sudo -S mkdir /home/' + adduser + '/www/log')
-    stdin, stdout, stderr = client.exec_command('sudo -S mkdir /home/' + adduser + '/www/web')
-    stdin, stdout, stderr = client.exec_command('sudo -S chown -R' + ' ' + adduser + ':' + adduser + ' ' + '/home/' + adduser )
-    stdin, stdout, stderr = client.exec_command('sudo -S echo' + ' ' + "'<VirtualHost *:80> \n"
-                                                                        "ServerName" + " " + domain + "\n"
-                                                                        "DocumentRoot /home/" + adduser + "/www/web/ \n"
-                                                                        "ErrorLog /home/" + adduser + "/www/log/dev_error.log \n"
-                                                                        "<Directory /home/" + adduser + "/www/web/> \n"
-                                                                        "Options Indexes FollowSymLinks \n"
-                                                                        "AllowOverride All \n"
-                                                                        "Order allow,deny \n"
-                                                                        "Allow from all \n"
-                                                                        "</Directory> '" + ' ' + '>' + ' ' + '/etc/apache2/sites-available/' + domain + '.conf')
-    stdin, stdout, stderr = client.exec_command('sudo -S a2ensite' + ' ' + domain + '.conf')
-    stdin, stdout, stderr = client.exec_command('sudo -S /etc/init.d/apache2 restart')
+    client.exec_command('mkdir /home/' + adduser + '/www')
+    client.exec_command('mkdir /home/' + adduser + '/www/log')
+    client.exec_command('mkdir /home/' + adduser + '/www/web')
+    client.exec_command('chown -R' + ' ' + adduser + ':' + adduser + ' ' + '/home/' + adduser )
+    client.exec_command('echo' + ' ' + "'<VirtualHost *:80> \n"
+                                        "ServerName" + " " + domain + "\n"
+                                        "DocumentRoot /home/" + adduser + "/www/web/ \n"
+                                        "ErrorLog /home/" + adduser + "/www/log/dev_error.log \n"
+                                        "<Directory /home/" + adduser + "/www/web/> \n"
+                                        "Options Indexes FollowSymLinks \n"
+                                        "AllowOverride All \n"
+                                        "Order allow,deny \n"
+                                        "Allow from all \n"
+                                        "</Directory> '" + ' ' + '>' + ' ' + '/etc/apache2/sites-available/' + domain + '.conf')
+    client.exec_command('a2ensite' + ' ' + domain + '.conf')
+    client.exec_command('/etc/init.d/apache2 restart')
     #result = stdout.read().splitlines()
 
     #print(result)
